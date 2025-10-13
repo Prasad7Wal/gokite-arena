7cfc26cf5710ecb950c431f20a0e74e01038477d
Prasad <115214702+Prasad7Wal@users.noreply.github.com>
2025-10-13 14:57:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const contractAddress = "PASTE_YOUR_CONTRACT_ADDRESS_HERE"; // Replace with your deployed contract
+const abi = [
+    "function joinArena() payable",
+    "function updateScore(uint256 _score)",
+    "function topPlayers() view returns (address[] memory, uint256[] memory)"
+];
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+let signer;
+
+async function init() {
+    await provider.send("eth_requestAccounts", []);
+    signer = provider.getSigner();
+    window.contract = new ethers.Contract(contractAddress, abi, signer);
+}
+
+document.getElementById("join").onclick = async () => {
+    const tx = await contract.joinArena({ value: ethers.utils.parseEther("0.01") });
+    await tx.wait();
+    alert("You joined the arena!");
+};
+
+document.getElementById("update").onclick = async () => {
+    const score = parseInt(document.getElementById("score").value);
+    const tx = await contract.updateScore(score);
+    await tx.wait();
+    alert("Score updated!");
+    loadLeaderboard();
+};
+
+async function loadLeaderboard() {
+    const [top, scores] = await contract.topPlayers();
+    const ul = document.getElementById("leaderboard");
+    ul.innerHTML = "";
+    for(let i=0;i<top.length;i++){
+        const li = document.createElement("li");
+        li.innerText = `${top[i]} : ${scores[i]} points`;
+        ul.appendChild(li);
+    }
+}
+
+init();
