54294f482cd533526ea22f8dfd845aabb6ead717
Prasad <115214702+Prasad7Wal@users.noreply.github.com>
2025-10-14 22:27:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -241,7 +241,7 @@ submitBtn.addEventListener("click", async ()=>{
   try{
     setStatus("⏳ Submitting score on-chain...");
     // Some contracts require value or not; here we send zero value for submission
-    await contract.submit(score, username, { value: ethers.utils.parseEther("0.001") });
+   const tx = await contract.submit(score, userDiscord, { value: ethers.utils.parseEther("0.001") });
 
     await tx.wait();
     setStatus("✅ Score submitted!");
